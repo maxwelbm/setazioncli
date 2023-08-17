628c17520fc6ada8f6f5c99cc02aac23252c826a
Maxwel Mazur <52722027+maxwelbm@users.noreply.github.com>
2023-08-17 09:12:17 -0300

    chore: rename name azioncli to azion

--- setup-azioncli.sh
+++ setup-azioncli.sh
@@ -11,16 +11,16 @@ fi
 
 echo binary download
 curl -o azioncli https://downloads.azion.com/stage/${environment}/x86_64/azion
-chmod 777 ./azioncli
+chmod 777 ./azion
 
 echo creating apps directory 
 sudo mkdir -p /opt/apps/
 
 echo move binary 
-sudo rm -rf /opt/apps/azioncli
-sudo mv ./azioncli /opt/apps/
+sudo rm -rf /opt/apps/azion
+sudo mv ./azion /opt/apps/
 
 echo create symbolic link
-sudo rm /usr/local/bin/azioncli
-sudo ln -s /opt/apps/azioncli /usr/local/bin/azioncli
+sudo rm /usr/local/bin/azion
+sudo ln -s /opt/apps/azion /usr/local/bin/azion
 
